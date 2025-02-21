14e531f415b975a2c829f89e1863534f572f600c
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-02-21 01:52:14 -0800

    3.4 Middleware: BandNameGenerator using Custom Middleware

--- 3.4 Middleware/index4.js
+++ 3.4 Middleware/index4.js
@@ -1,7 +1,33 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandName = "";
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+function bandNameGenerator(req, res, next){
+  console.log(req.body);
+  bandName = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(bandNameGenerator);
+
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req,res) =>{
+  console.log(req.body);
+  res.send(`<h1>Your band Name is:</h1><h2>${bandName}.`)
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
