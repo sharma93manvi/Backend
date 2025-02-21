61a36f74377a4faf88de869c4b83b30a59a14354
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-02-20 17:00:52 -0800

    Server is able to handle /, contact, /about endpoints

--- 3.2 HTTP Requests/index.js
+++ 3.2 HTTP Requests/index.js
@@ -7,6 +7,16 @@ app.get("/", (req, res) => {
     console.log(req.rawHeaders);
 });
 
+app.get("/contact", (req, res) => {
+    res.send("Contact us at abc@gmail.com");
+    console.log(req.rawHeaders);
+});
+
+app.get("/about", (req, res) => {
+    res.send("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus consectetur elit, vitae fringilla lorem aliquet non. Sed porttitor fermentum tincidunt. ");
+    console.log(req.rawHeaders);
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`)
 });
