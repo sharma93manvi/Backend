ebbe52493f2764203427ec0818bb5e36b06344ef
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-02-22 16:10:47 -0800

    4.0 EJS: Add Conditional loop

--- 4.0 EJS/index.js
+++ 4.0 EJS/index.js
@@ -6,10 +6,20 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
+    const today = new Date();
+    const day = today.getDay();
 
+    // console.log(day);
+    let type = "a weekday";
+    let adv = "it's time to work hard";
+
+    if(day===0 || day === 6){
+        type = "a weekend";
+        adv = "it's time to have fun";
+    }
     res.render("index.ejs", {
-        dayType: "a weekday",
-        advice: "it's time to work hard",
+        dayType: type,
+        advice: adv,
     });
 });
 
