53444b602b0db158133596c88b89132f3ad94738
Manvi Sharma <manvisharma@Manvis-MacBook-Pro.local>
2025-03-13 23:16:55 -0700

    Updated Register Page

--- 9.1+Authentication+Lv.1/index.js
+++ 9.1+Authentication+Lv.1/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  host: "localhost",
+  database: "secrets",
+  user: "postgres",
+  password: "Welcome@123",
+  port: 5432
+})
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -11,7 +21,7 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
-app.get("/login", (req, res) => {
+app.get("/login", async (req, res) => {
   res.render("login.ejs");
 });
 
@@ -19,9 +29,35 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
 
-app.post("/login", async (req, res) => {});
+  try{
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1",[email,])
+    
+    if(checkResult.rows.length > 0){
+      res.send("Email already exists. Try logging in.");
+    }else{
+      const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, password]);
+    }
+    console.log(result);
+    res.render("secrets.ejs");
+
+  }catch(err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+   // try{
+
+  // }catch(err) {
+  //   console.log(err);
+  // }
+  const email = req.body.username;
+  const password = req.body.password;
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
